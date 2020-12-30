1e324b403fe69b4f99a62ac94a432d4c6f9a3ea3
RunFridge <hwhang0917@gmail.com>
2020-12-30 11:19:15 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+
+const appIndex = path.resolve(__dirname, "src", "index.tsx");
+const appDist = path.resolve(__dirname, "dist");
+const appHtml = path.resolve(__dirname, "public", "index.html");
+
+module.exports = function (webpackEnv) {
+  const isEnvDevelopment = webpackEnv.development ? true : false;
+  const isEnvProduction = webpackEnv.production ? true : false;
+
+  return {
+    mode: isEnvDevelopment ? "development" : isEnvProduction && "production",
+    entry: appIndex,
+    output: {
+      path: appDist,
+      filename: "bundle.js",
+      chunkFilename: "[name].js",
+    },
+    cache: {
+      type: "memory",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: isEnvDevelopment,
+            },
+          },
+        },
+      ],
+    },
+    optimization: isEnvDevelopment
+      ? false
+      : isEnvProduction && {
+          minimize: true,
+        },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: "React + TS template",
+        template: appHtml,
+      }),
+      new WebpackManifestPlugin(),
+    ],
+    devtool: "source-map",
+    devServer: {
+      contentBase: appDist,
+      inline: true,
+      host: "localhost",
+      port: 3000,
+    },
+  };
+};
