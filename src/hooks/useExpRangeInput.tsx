980d115e1ec4150cd8abd3fec75ed3295a47e3e4
Heesang whang <hwhang0917@gmail.com>
2021-01-11 11:51:42 +0900

    :recycle: Refactor useExpRangeInput hook

--- src/hooks/useExpRangeInput.tsx
+++ src/hooks/useExpRangeInput.tsx
@@ -1,37 +1,59 @@
 import { useState } from "react";
 
+class ExponentCalculator {
+  private WEIGHT = Math.log(1000) / 100;
+  getExpOutFromExpBase(expBase: number): number {
+    return Math.round(Math.exp(this.WEIGHT * expBase));
+  }
+  getExpBaseFromExpOut(expOut: number): number {
+    return Math.log(expOut) / this.WEIGHT;
+  }
+}
+
 function useExpRangeInput(): [
   number,
   number,
-  (arg: React.ChangeEvent<HTMLInputElement>) => void,
-  (arg: React.ChangeEvent<HTMLInputElement>) => void
+  (event: React.ChangeEvent<HTMLInputElement>) => void,
+  (event: React.ChangeEvent<HTMLInputElement>) => void,
+  (outMax: number) => number
 ] {
+  // States
   const [expBase, setExpBase] = useState<number>(0);
   const [expOut, setExpOut] = useState<number>(0);
 
-  const ExpWeight = Math.log(1000) / 100;
+  // Calculator
+  const Calculator = new ExponentCalculator();
 
+  // onInput Event listener
   const onExpBaseChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { value },
     } = event;
-    setExpBase(parseInt(value));
 
-    const newExpOut = Math.exp(ExpWeight * parseInt(value));
-    setExpOut(Math.round(newExpOut));
+    const expBase = parseInt(value);
+    setExpBase(expBase);
+
+    const newExpOut = Calculator.getExpOutFromExpBase(expBase);
+    setExpOut(newExpOut);
   };
 
+  // onInput Event listener
   const onExpOutChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { value },
     } = event;
-    setExpOut(parseFloat(value));
+    const expOut = parseFloat(value);
+    setExpOut(expOut);
 
-    const newExpBase = Math.log(parseFloat(value)) / ExpWeight;
+    const newExpBase = Calculator.getExpBaseFromExpOut(expOut);
     setExpBase(newExpBase);
   };
 
-  return [expBase, expOut, onExpBaseChange, onExpOutChange];
+  const calculateRangeMax = (outMax: number): number => {
+    return Math.round(Calculator.getExpBaseFromExpOut(outMax));
+  };
+
+  return [expBase, expOut, onExpBaseChange, onExpOutChange, calculateRangeMax];
 }
 
 export default useExpRangeInput;
